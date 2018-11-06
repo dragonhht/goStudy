c6b8e2d24a82ab8e8921fb8dc7a2efa9d48096a5
dragonhht <dragon_hht@163.com>
2018-11-07 00:27:35 +0800

    变量

--- src/main.go
+++ src/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	. "time"
 )
 
-var count int
+var count int = 32
 var flo complex64
 
+var a, b, c, d int = 1, 2, 3, 4
+
+var (
+	s1 int
+	s2 uint
+	s3 string
+	s4 bool
+)
+
 func main() {
-	fmt.Println(count)
+	n1, n2 := 2, 3
+	fmt.Print(n1 + n2)
+	fmt.Println(reflect.TypeOf(a))
+	var z = int64(a)
+	fmt.Println(reflect.TypeOf(z))
 	fmt.Println(Time{})
 }
