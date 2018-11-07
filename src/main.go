8442cdeeb71fff5882e6871012cc988a06ab3023
dragonhht <dragon_hht@163.com>
2018-11-07 18:17:41 +0800

    -

--- src/main.go
+++ src/main.go
@@ -9,6 +9,8 @@ import (
 var count int = 32
 var flo complex64
 
+const testCount, d1, d3  = 33, "Hello", true
+
 var a, b, c, d int = 1, 2, 3, 4
 
 var (
@@ -25,4 +27,6 @@ func main() {
 	var z = int64(a)
 	fmt.Println(reflect.TypeOf(z))
 	fmt.Println(Time{})
+	fmt.Println(&n1)
+	fmt.Println(testCount)
 }
